8727f0198f267262cf153f9e28f481ec929b5498
dhphuongg <hoangphuong270703@gmail.com>
2025-05-13 21:15:19 +0700

    fix: build apk

--- build-apk.sh
+++ build-apk.sh
@@ -3,6 +3,6 @@ then
 	echo "$0 - require build number"
 else
   echo $1
-  flutter build apk --flavor Staging --build-number=$1
+  flutter build apk --flavor Dev --build-number=$1
   cp build/app/outputs/flutter-apk/app-staging-release.apk ~/Desktop/babilon_1.$1.apk
 fi
